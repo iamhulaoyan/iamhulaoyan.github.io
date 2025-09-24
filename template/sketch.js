37013d8a12a9e0430c95c41d5a1b04412438fa66
iamhulaoyan <karlo42307@gmail.com>
2025-09-24 19:23:11 -0400

    Add files via upload

---
+++ template/sketch.js
@@ -0,0 +1,13 @@
+//The setup function only happens once
+function setup() {
+	createCanvas(500, 500); //create a 500px X 500px canvas
+}
+
+//The draw function happens over and over again
+function draw() {
+  background(173,216,230); //an RGB color for the canvas' background
+  //circle
+  stroke(255,255,127) // an RGB color for the circle's border
+  fill(255,255,127,150); // an RGB color for the inside of the circle (the last number refers to transparency (min. 0, max. 255))
+  ellipse(width/2,height/2,20,20); // center of canvas, 20px dia
+}
